2cb85063716fed75e2ee5f58a22b44ef454099d5
docaothangg <94737261+docaothangg@users.noreply.github.com>
2025-05-03 01:39:33 +0700

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,11 +1,6 @@
 import mongoose from "mongoose";
 export const connectDB = async () => {
-<<<<<<< HEAD
-    await mongoose.connect('mongodb+srv://dcthang489:thang123@cluster0.k9jbh.mongodb.net/Final').then(() => console.log("DB connected"));
-    //await mongoose.connect('mongodb+srv://tranvantin2009:kltt42@kltt42.erl2l.mongodb.net/KLTT_42').then(() => console.log("DB connected"));
-}   
-=======
-    await mongoose.connect("mongodb+srv://dcthang489:Thang123@cluster0.k9jbh.mongodb.net/Final").then(() => console.log("DB connected"));
-    //await mongoose.connect('mongodb+srv://tranvantin2009:kltt42@kltt42.erl2l.mongodb.net/KLTT_42').then(()=>console.log("DB connected"));
+
+    //await mongoose.connect("mongodb+srv://dcthang489:Thang123@cluster0.k9jbh.mongodb.net/Final").then(() => console.log("DB connected"));
+    await mongoose.connect('mongodb+srv://tranvantin2009:kltt42@kltt42.erl2l.mongodb.net/KLTT_42').then(()=>console.log("DB connected"));
 }
->>>>>>> c1949cc (Bao cao lan 3)
